78dead09782e799e3fb76022a09174a2535ffa08
Mafffie Cohen <111717714+maffiemaffie@users.noreply.github.com>
2023-03-02 10:22:38 -0500

    starting to put together movement classes
    hi maffie

--- hi maffie.js
+++ hi maffie.js
@@ -1,4 +1,18 @@
 class Wrapper {
+    departureTime;
+    arrivalTime;
+
+    EASING;
+    MAX_SPEED;
+
+    target;
+
+    physicsGuys = {
+        'position': {'x': 0, 'y': 0},
+        'velocity': {'x': 0, 'y': 0},
+        'acceleration': {'x': 0, 'y': 0}
+    }
+
     updateValues(position, velocity, deltaTime) {
 
     }
@@ -72,3 +86,59 @@ class Wrapper {
  * Yo a (computationally) faster way to do this would probably be to save the acceleration and then calculate just the position each time btw lol 
  */
 
+class AnimationPath {
+    departureTime;
+    arrivalTime;
+
+    EASING;
+    MAX_SPEED;
+
+    target;
+
+    // FunctionPaths
+    position;
+    velocity;
+    acceleration;
+
+    set position(value) { throw 'position is readonly' };
+    set velocity(value) { throw 'velocity is readonly' };
+    set acceleration(value) { throw 'acceleration is readonly' };
+
+    constructor(origin, target, initialVelocity, maxSpeed, easing) {
+        // calculate accelerations
+        const calculateAcceleration = (origin, target, initialVelocity, maxSpeed, easing) => {
+            const [xbx, xby, xax, xay, vx, vy, s, t] = [target.x, target.y, origin.x, origin.y, initialVelocity.x, initialVelocity.y, maxSpeed, easing];
+            const a1x = -vx / t;
+            const a1y = -vy / t;
+
+            const scale = t * Math.sqrt(
+                (xbx - (0.5 * a1x * t - vx) * t - xax)**2 + 
+                (xby - (0.5 * a1y * t - vy) * t - xay)**2);
+
+            const a2x = s * (xbx - (0.5 * a1x * t - vx) * t - xax) * scale;
+            const a2y = s * (xby - (0.5 * a1y * t - vy) * t - xay) * scale;
+
+            return {
+                'x': a1x + a2x,
+                'y': a1y + a2y
+            };
+        }
+        const accelerationEaseIn = calculateAcceleration(origin, target, initialVelocity, maxSpeed, easing);
+        const accelerationLinear = 0;
+
+        // calculate velocity functions
+
+        // calculate position functions
+    }
+}
+
+class FunctionPath {
+    pathFunction;
+    constructor(pathFunction) {
+        this.pathFunction = pathFunction;
+    }
+
+    at(timeSeconds) {
+        return this.pathFunction(timeSeconds);
+    }
+}
